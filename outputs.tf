91edea6ac99ffb96902309710938e3eda4efeec7
dantribulations090 <41971277+danielkowalski1@users.noreply.github.com>
2023-06-06 16:07:27 -0500

    comment outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,6 @@
-output "instance_ami" {
-  #value = aws_instance.blog.ami
-  value = aws_instance.web.arn
-}
-output "instance_arn" {
-  #value = aws_instance.blog.arn
-  value = aws_instance.web.arn
-}
+#output "instance_ami" {
+#  value = aws_instance.blog.ami
+#}
+#output "instance_arn" {
+#  value = aws_instance.blog.arn
+#}
